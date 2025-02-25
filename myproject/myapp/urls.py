8f825b8522756e2cc425417edcb03eb21635e6ea
Sofiia Kostereva <kosterewa.sofia@gmail.com>
2025-02-25 20:12:26 +0200

    new commit

--- myproject/myapp/urls.py
+++ myproject/myapp/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='index.html'),
+    path('', views.index, name='index'),  # Кореневий URL
 ]
--- myproject/myapp/views.py
+++ myproject/myapp/views.py
@@ -1,4 +1,9 @@
 from django.http import HttpResponse
+from django.shortcuts import render
+import os
 
 def index(request):
-    return HttpResponse("Вітаємо на сторінці index!")
+    with open(os.path.join('myapp/static/html/index.html'), 'r') as f:
+        return HttpResponse(f.read())
+def index(request):
+    return render(request, 'index.html')
